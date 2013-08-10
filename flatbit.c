dbd1574bbd20489c56bb91b967ea4baf6119965a
Anssi Etelaniemi <aetelani@hotmail.com>
2013-08-11 01:10:47 +0300

    Added license

--- flatbit.c
+++ flatbit.c
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2013  Anssi Eteläniemi
+
+This file is part of FlatBit.
+
+FlatBit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FlatBit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FlatBit.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "flatbit.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -37,7 +55,7 @@ Container * makeContainer(FBStorage * s)
 
 int writeHeader(FBStorage * storage)
 {
-    //if (container)
+    if (storage)
     {
 		lseek((int) storage->handle, (off_t) 0, SEEK_SET);
 		fwrite(&fbHeader, sizeof(Header), 1, storage->handle);
