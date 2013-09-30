8f3abe5ee63f684268d12d725ae3487239b23a40
Anssi Etelaniemi <aetelani@live.com>
2013-09-30 17:18:43 +0300

    takin' smokes on hull recursion

--- tmp/ch/cv.c
+++ tmp/ch/cv.c
@@ -14,31 +14,60 @@ static const int dimxy = 100;
 
 int cmp_points(const void * lhs, const void * rhs)
 {
-	return (((struct point *)lhs)->y > ((struct point *)rhs)->y);
+	return (((struct point *)lhs)->x > ((struct point *)rhs)->x);
 }
 
-int ccw(struct point * p1, struct point * p2, struct point * p3)
+int ccw(struct point p1, struct point p2, struct point p3)
 {
-	return (p2->x-p1->x)*(p3->y-p1->y) - (p2->y-p1->y)*(p3->x-p1->x);
+	int ret = (p2.x-p1.x)*(p3.y-p1.y) - (p2.y-p1.y)*(p3.x-p1.x);
+	printf("(%d,%d):ccw:%d\n", (p2.x-p1.x)*(p3.y-p1.y), (p2.y-p1.y)*(p3.x-p1.x), 0<ret);
+	return ret;
 }
 
+struct hull_point {
+	struct point * p;
+	struct hull_point * next;
+	int ccw;
+};
 
 int main()
 {
 	FILE * plotter = popen ("gnuplot -p", "w");	
-	fprintf(plotter, "set autoscale; plot '-'\n");
+	fprintf(plotter, "set autoscale; plot '-', '-'\n");
 	srand(time(NULL));
 
-	points = (point*) calloc(points_count, sizeof(struct point));	
+	points = (point*) calloc(points_count, sizeof(struct point));
+	struct hull_point * hull_first, * hull = (hull_point*) calloc(1, sizeof(hull_point));
+	hull_first = hull;
 	
 	for(int i = 0; i < points_count; i++)
 	{
-		points[i].x = rand()%dimxy;
-		points[i].y = rand()%dimxy;
+		points[i].x = rand()%dimxy*10;
+		points[i].y = rand()%dimxy*10;
 	}
 	
 	qsort(points, points_count, sizeof(struct point), cmp_points);
 	
+	for(int i = 1, j=0; i < points_count; i++)
+	{
+		//printf("(%d,%d)-(%d,%d),(%d,%d):ccw:%d\n", points[i-1].x, points[i-1].y, points[i].x, points[i].y, points[i+1].x, points[i+1].y, !!ccw(points[i-1], points[i], points[i+1]));
+		j=i;
+		while (j+1 < points_count && ccw(points[i-1], points[j], points[j+1]) && j++)
+		{
+			hull->p = &(points[j+1]);
+			hull->ccw = 1;
+			hull->next = (hull_point*) calloc(1, sizeof(hull_point));
+			hull = hull->next;
+		}
+	}
+	struct hull_point * u;
+	u = hull_first;
+	while (u && u->p) {
+		printf("hull:%d,%d\n", u->p->x, u->p->y);
+		u = u->next;
+	}
+	
+	fprintf(plotter, "%d %d w p 1 -1\ne\n", points[0].x, points[0].y);
 	for(int i = 0; i < points_count; i++)
 	{
 		fprintf(plotter, "%d %d\n", points[i].x, points[i].y);
