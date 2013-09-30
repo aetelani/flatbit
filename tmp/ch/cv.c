e68515c50f37a6ab085d58f5dfe31979b30641c1
Anssi Etelaniemi <aetelani@hotmail.com>
2013-09-30 22:50:45 +0300

    convex hull plotter

--- tmp/ch/cv.c
+++ tmp/ch/cv.c
@@ -1,80 +1,85 @@
-#include <stdio.h>
+#include <stdio.h>  
 #include <stdlib.h>
 #include <time.h>
 
 struct point {
 	int x, y;
+    int hull;
 };
 struct point * upper_hull;
 struct point * lower_hull;
 struct point * points;
-static const int points_count = 10;
+static const int points_count = 50;
 static const int dimxy = 100;
 
 
 int cmp_points(const void * lhs, const void * rhs)
 {
-	return (((struct point *)lhs)->x > ((struct point *)rhs)->x);
+    if (((struct point *)lhs)->x == ((struct point *)rhs)->x)
+        return ((struct point *)lhs)->y == ((struct point *)rhs)->y;
+    else
+        return (((struct point *)lhs)->x > ((struct point *)rhs)->x);
 }
 
 int ccw(struct point p1, struct point p2, struct point p3)
 {
 	int ret = (p2.x-p1.x)*(p3.y-p1.y) - (p2.y-p1.y)*(p3.x-p1.x);
 	printf("(%d,%d):ccw:%d\n", (p2.x-p1.x)*(p3.y-p1.y), (p2.y-p1.y)*(p3.x-p1.x), 0<ret);
-	return ret;
+	return ret > 0;
 }
-
-struct hull_point {
-	struct point * p;
-	struct hull_point * next;
-	int ccw;
-};
 
 int main()
 {
-	FILE * plotter = popen ("gnuplot -p", "w");	
-	fprintf(plotter, "set autoscale; plot '-', '-'\n");
 	srand(time(NULL));
 
 	points = (point*) calloc(points_count, sizeof(struct point));
-	struct hull_point * hull_first, * hull = (hull_point*) calloc(1, sizeof(hull_point));
-	hull_first = hull;
-	
+	struct point * hull = (point*) calloc(points_count, sizeof(struct point));
+
 	for(int i = 0; i < points_count; i++)
 	{
-		points[i].x = rand()%dimxy*10;
-		points[i].y = rand()%dimxy*10;
+		points[i].x = rand()%dimxy;
+		points[i].y = rand()%dimxy;
 	}
-	
-	qsort(points, points_count, sizeof(struct point), cmp_points);
-	
-	for(int i = 1, j=0; i < points_count; i++)
+
+    qsort(points, points_count, sizeof(struct point), cmp_points);
+
+    int hind = 0;
+	for(int i=0; i < points_count; i++)
 	{
-		//printf("(%d,%d)-(%d,%d),(%d,%d):ccw:%d\n", points[i-1].x, points[i-1].y, points[i].x, points[i].y, points[i+1].x, points[i+1].y, !!ccw(points[i-1], points[i], points[i+1]));
-		j=i;
-		while (j < points_count && !ccw(points[i-1], points[j], points[j+1]) && j++) {};
-		
-		if (j < points_count)
-		{
-			hull->p = &(points[j+1]);
-			hull->ccw = 1;
-			hull->next = (hull_point*) calloc(1, sizeof(hull_point));
-			hull = hull->next;
-		}
+		while (hind >= 2 && !ccw(hull[hind-2], hull[hind-1], points[i])) {hind--;}
+        hull[hind++] = points[i];
 	}
-	struct hull_point * u;
-	u = hull_first;
-	while (u && u->p && u->p != &points[0]) {
-		printf("hull:%d,%d, turn:%d\n", u->p->x, u->p->y, u->ccw);
-		u = u->next;
+
+     for(int i = points_count-2; i >= 0; --i)
+     {
+        while (hind >= 2 && !ccw(hull[hind-2], hull[hind-1], points[i])) {hind--;}
+        hull[hind++] = points[i];
+    } 
+
+    printf("hull:");
+	for (int i=0; i < hind; i++)
+    {
+        printf("(%d,%d),", hull[i].x, hull[i].y);
 	}
-	
-	fprintf(plotter, "%d %d w p 1 -1\ne\n", points[0].x, points[0].y);
+    printf("\n");
+
+
+    FILE * plotter = popen ("gnuplot -p ", "w");	
+	fprintf(plotter, "set autoscale; plot '-' w p pt 1 ps 2, '-' w p lc 0, '-' w lp pt 6 ps 3;\n");
+	fprintf(plotter, "%d %d\ne\n", points[0].x, points[0].y);
 	for(int i = 0; i < points_count; i++)
 	{
 		fprintf(plotter, "%d %d\n", points[i].x, points[i].y);
 	}
 	fprintf(plotter, "e\n");
+    
+	fprintf(plotter, "%d %d\n", points[0].x, points[0].y);
+	for(int i = 0; i < hind; i++)
+	{
+		fprintf(plotter, "%d %d\n", hull[i].x, hull[i].y);
+	}
+    fprintf(plotter, "e\n");
+    
 	pclose(plotter);
 	free(points);
 	
