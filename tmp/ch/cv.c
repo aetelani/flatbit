a78cf31c6fbf66a305e61e4570f733dbed7acdd6
Anssi Etelaniemi <aetelani@live.com>
2013-10-01 12:38:19 +0300

    new plot function. fixed index

--- tmp/ch/cv.c
+++ tmp/ch/cv.c
@@ -4,27 +4,25 @@
 
 struct point {
 	int x, y;
-    int hull;
 };
 struct point * upper_hull;
 struct point * lower_hull;
 struct point * points;
-static const int points_count = 50;
-static const int dimxy = 100;
+static const int points_count = 500;
+static const int dimxy = 1000;
 
+int plot(struct point * points, int points_count, struct point * hull, int hind);
 
 int cmp_points(const void * lhs, const void * rhs)
 {
-    if (((struct point *)lhs)->x == ((struct point *)rhs)->x)
-        return ((struct point *)lhs)->y == ((struct point *)rhs)->y;
-    else
-        return (((struct point *)lhs)->x > ((struct point *)rhs)->x);
+    return ((struct point *)lhs)->x == ((struct point *)rhs)->x ?
+			((struct point *)lhs)->y > ((struct point *)rhs)->y :
+			((struct point *)lhs)->x > ((struct point *)rhs)->x ;
 }
 
 int ccw(struct point p1, struct point p2, struct point p3)
 {
 	int ret = (p2.x-p1.x)*(p3.y-p1.y) - (p2.y-p1.y)*(p3.x-p1.x);
-	printf("(%d,%d):ccw:%d\n", (p2.x-p1.x)*(p3.y-p1.y), (p2.y-p1.y)*(p3.x-p1.x), 0<ret);
 	return ret > 0;
 }
 
@@ -50,21 +48,25 @@ int main()
         hull[hind++] = points[i];
 	}
 
-     for(int i = points_count-2; i >= 0; --i)
+     for(int i = points_count-2, lim = hind+1; i >= 0; --i)
      {
-        while (hind >= 2 && !ccw(hull[hind-2], hull[hind-1], points[i])) {hind--;}
+        while (hind >= lim && !ccw(hull[hind-2], hull[hind-1], points[i])) {hind--;}
         hull[hind++] = points[i];
     } 
 
-    printf("hull:");
-	for (int i=0; i < hind; i++)
-    {
-        printf("(%d,%d),", hull[i].x, hull[i].y);
-	}
-    printf("\n");
+    /*printf("hull:");
+	for (int i=0; i < hind; i++) { printf("(%d,%d),", hull[i].x, hull[i].y); }
+    printf("\n");*/
+    
+	int ret = plot(points, points_count, hull, hind);
+	
+	free(points);
+	
+}
 
-
-    FILE * plotter = popen ("gnuplot -p ", "w");	
+int plot(struct point * points, int points_count, struct point * hull, int hind)
+{
+	FILE * plotter = popen ("gnuplot -p ", "w");	
 	fprintf(plotter, "set autoscale; plot '-' w p pt 1 ps 2, '-' w p lc 0, '-' w lp pt 6 ps 3;\n");
 	fprintf(plotter, "%d %d\ne\n", points[0].x, points[0].y);
 	for(int i = 0; i < points_count; i++)
@@ -81,6 +83,4 @@ int main()
     fprintf(plotter, "e\n");
     
 	pclose(plotter);
-	free(points);
-	
 }
