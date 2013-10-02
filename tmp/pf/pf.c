275b031d737597e32e2a53a456a848b1a1de46d4
Anssi Etelaniemi <aetelani@live.com>
2013-10-02 16:29:09 +0300

    chagned edges in point struct. custom label in gnuplot

--- tmp/pf/pf.c
+++ tmp/pf/pf.c
@@ -3,11 +3,15 @@
 #include <time.h>
 #include <sys/param.h>
 
+struct point;
+struct edge {
+	struct point * e;
+	int cost;
+};
+
 struct point {
 	int x, y, z;
-};
-struct edge {
-	int cost;
+	struct edge edges[8];
 };
 
 int cmp_points(const void * lhs, const void * rhs)
@@ -22,32 +26,32 @@ int plot(struct point * points, int points_count, struct point * path, int steps
 	FILE * plotter = popen ("gnuplot -p ", "w");
 	fprintf(plotter, "set autoscale;\n");
 	fprintf(plotter, "set pm3d map;\n");
-	fprintf(plotter, "splot '-' w p pt 6 ps 0.3 lc rgb variable, '-' w l lc rgb variable;\n"); //, '-' w p lc 0, '-' w lp pt 6 ps 3;\n");
+//	fprintf(plotter, "splot '-' w p pt 6 ps 0.3 lc rgb variable, '-' w l lc rgb variable;\n"); //, '-' w p lc 0, '-' w lp pt 6 ps 3;\n");
+	fprintf(plotter, "splot '-' w labels tc rgb variable, '-' w l lc rgb variable;\n"); //, '-' w p lc 0, '-' w lp pt 6 ps 3;\n");
 
-	//fprintf(plotter, "%d %d\ne\n", points[0].x, points[0].y);
 	for(int i = 0; i < points_count; i++)
 	{
-		fprintf(plotter, "%d %d %d 0x000000\n", points[i].x, points[i].y, points[i].z);
+		fprintf(plotter, "%d %d %d %c 0xaf0a00\n", points[i].x, points[i].y, points[i].z, (points[i].z==INT_MAX?0xf8:points[i].z));
 	}
 	fprintf(plotter, "e\n");
     
 	fprintf(plotter, "%d %d %d\n", points[0].x, points[0].y, points[0].z);
 	for(int i = 0; i < steps; i++)
 	{
-		fprintf(plotter, "%d %d %d 0xaf0000\n", path[i].x, path[i].y, path[i].z);
+		fprintf(plotter, "%d %d %d 0xFF00FF\n", path[i].x, path[i].y, path[i].z);
 	}
     fprintf(plotter, "e\n");
     pclose(plotter);
 }
 
 int iterNeighbors(struct point * arr, int i, int j, int dim) {
-	int rowLimit = dim-1;
-	int colLimit = dim-1;
-	int count = 0;
+	int rowLimit = dim-1, colLimit = dim-1, count = 0;
 	for(int x = MAX(0, i-1); x <= MIN(i+1, rowLimit); x++) {
 		for(int y = MAX(0, j-1); y <= MIN(j+1, colLimit); y++) {
-			if(x != i || y != j) {
-				printf("(%d,%d,%d)\n",x, y, arr[x*dim+j].z);
+			if(x != i && y != j) {
+				printf("(%d,%d - %d)~%d\n",x, y, x*dim+y, arr[x*dim+y].z);
+				arr[x*dim+y].edges[count].e = &arr[x*dim+y];
+				arr[x*dim+y].edges[count].cost = rand()%3;
 				count++;
 			}
 		}
@@ -62,11 +66,11 @@ int matrix_points(struct point ** pts, const int count, const int dim)
 	int i,j, edge_count;
 	const int corner = 3, side = 5, normal = 8;
 	for(i = 0, j = 0, edge_count = 0; i < count;) {
-		for(j=0;j<dim;j++)  {
+		for(j=0; j < dim; j++)  {
 			(*pts)[i+j].x = i/dim;
 			(*pts)[i+j].y = j;
 			(*pts)[i+j].z = INT_MAX;
-			//printf("(%d,%d,%d)\n", (*pts)[j].x, (*pts)[j].y, (*pts)[j].z);
+			printf("(%d,%d,%d)\n", (*pts)[i+j].x, (*pts)[i+j].y, (*pts)[i+j].z);
 		}
 		i += j;
 	}
@@ -74,8 +78,9 @@ int matrix_points(struct point ** pts, const int count, const int dim)
 }
 
 struct point * points, * beg, * end;
-static const int dim = 10;
-static const int points_count = 10 * 10;
+static const int dim = 10; // #x
+static const int points_count = 10 * 10; // dim^2
+static int edge_count = 10 * 10 * 8; // points_count * neighbor count
 
 int pf(int a, int b)
 {
